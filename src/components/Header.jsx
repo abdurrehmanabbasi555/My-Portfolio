fd836b6ad905f34023366990d726f2324dc2b9d0
abdurrehman abbasi <abdurrehman.wsb@gmail.com>
2025-12-17 00:02:50 +0500

    Fixed mobile responsive issues

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,18 +21,8 @@ export default function Example() {
     >
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
-          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-            {/* Mobile menu button*/}
-            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-white/5 hover:text-white focus:outline-2 focus:-outline-offset-1 focus:outline-indigo-500">
-              <span className="absolute -inset-0.5" />
-              <span className="sr-only">Open main menu</span>
-              <Bars3Icon aria-hidden="true" className="block size-6 group-data-open:hidden" />
-              <XMarkIcon aria-hidden="true" className="hidden size-6 group-data-open:block" />
-            </DisclosureButton>
-          </div>
-          <div className="flex flex-1 items-center sm:items-stretch justify-between">
-            <div className="flex shrink-0 items-center">
-              <a href="home">
+          <div className="flex shrink-0 items-center">
+              <a href="#home">
               <img
           src={profile}
           alt="Profile"
@@ -40,6 +30,17 @@ export default function Example() {
         />
         </a>
             </div>
+          
+          <div className="flex sm:flex-0 items-center justify-between sm:items-stretch">
+          <div className="flex items-center sm:hidden">
+            {/* Mobile menu button*/}
+            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-white/5 hover:text-white focus:outline-2 focus:-outline-offset-1 focus:outline-indigo-500">
+              <span className="absolute -inset-0.5" />
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon aria-hidden="true" className="block size-6 group-data-open:hidden" />
+              <XMarkIcon aria-hidden="true" className="hidden size-6 group-data-open:block" />
+            </DisclosureButton>
+          </div>  
             <div className="hidden sm:ml-6 sm:block mt-4">
               <div className="flex space-x-4">
                 {navigation.map((item) => (
@@ -69,7 +70,7 @@ export default function Example() {
               href={item.href}
               aria-current={item.current ? 'page' : undefined}
               className={classNames(
-                item.current ? 'bg-gray-950/50 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white',
+                item.current ? 'bg-gray-950/50 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-[#01ce90] hover:underline',
                 'block rounded-md px-3 py-2 text-base font-medium',
               )}
             >
